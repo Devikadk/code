259d9fe972a9ddf83247e15485b884ec80ac50a0
Devikadk <devikakrishnan249@gmail.com>
2020-10-01 09:17:18 +0530

    Update bmd_parse_main.c

--- esb_app/src/BMD/bmd_parse_main.c
+++ esb_app/src/BMD/bmd_parse_main.c
@@ -5,6 +5,11 @@
 #include "xml_header.h"
 #include <libxml/parser.h>
 
+ /* @ brief : extracting payload from xml file
+  * input arguments :file(where xml file is stored)
+  * returns payload as char *
+  * returning payload
+  */
 bmd *bmd_main_parse(char * file)
 {
     bmd * b = (bmd*) malloc (sizeof(bmd));
