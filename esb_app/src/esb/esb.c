cd2e78975ca6c4227a95669222c31de03c9773b0
Devikadk <devikakrishnan249@gmail.com>
2020-10-01 09:53:08 +0530

    Update esb.c

--- esb_app/src/esb/esb.c
+++ esb_app/src/esb/esb.c
@@ -13,7 +13,7 @@ int queue_the_request(bmd *b,char *file)
     int success = 1; // 1 => OK, -1 => Error cases
     int failure=-1;
     /** 
-     * TODO: Insert the envelop data into esb_requests table,
+    * Insert the envelop data into esb_requests table,
      * and implement other logic for enqueueing the request
      * as specified in Theory of Operation.
      */
@@ -32,7 +32,7 @@ int queue_the_request(bmd *b,char *file)
 int process_esb_request(char* file) {
     int status = 1; // 1 => OK, -ve => Errors
   //  printf("Handling the BMD %s\n", bmd_file_path);
-    /** TODO: 
+    /**  
      * Perform the steps outlined in the Theory of Operation section of
      * the ESB specs document. Each major step should be implemented in
      * a separate module. Suitable unit tests should be created for all
